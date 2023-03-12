a4b8b93bed3d585eca30b2fcfef1757fd998d483
Rcthapa1996 <Rcthapa1996@gmail.com>
2023-03-12 18:33:26 +0545

    add bundle" "

--- src/js/dom-loader.js
+++ src/js/dom-loader.js
@@ -1,2 +1,4 @@
-var secretButton = document.querySelector('#secret-button');
-var secretParagraph = document.querySelector('#secret-paragraph');
+var secretButton = document.querySelector("#secret-button");
+var secretParagraph = document.querySelector("#secret-paragraph");
+export { secretButton, secretParagraph };
+// module.export = { secretButton, secretParagraph };
